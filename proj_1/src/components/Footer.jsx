b9d61df93abb53278d23c6a513e2e6845efd6403
Mustafa <yusufmustufa@gmail.com>
2024-02-18 17:10:38 +0530

    Started footer

--- proj_1/src/components/Footer.jsx
+++ proj_1/src/components/Footer.jsx
@@ -1,8 +1,39 @@
 /* import React from 'react'*/
+import { BsArrowUpRightSquare } from "react-icons/bs";
+import {Link} from "react-router-dom";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+    <>
+    <div className="div_foot">
+      <div>
+        <div className="col_1">
+          <h1>MyFarma</h1>
+          <div>
+              <p></p>
+          </div>
+        </div>
+
+        <div className="col_2">
+            <h1>Explore</h1>
+            <Link to={"/"}>Buy/Sell</Link>
+            <Link to={"/contact"}>Contact</Link>
+            <Link to={"/contact"}>Guidlines</Link>
+        </div>
+
+        <div className="col_3">
+            <h1>Contact Us</h1>
+            <main>
+              <a href="">Github</a>
+              <a href="">Instagram</a>
+              <a href="">Email</a>
+              <a href="">Sponsor us <BsArrowUpRightSquare/> </a>
+            </main>
+
+        </div>
+      </div>
+    </div>
+    </>
+  );
 }
 
 export default Footer
