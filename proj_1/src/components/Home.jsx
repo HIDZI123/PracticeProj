3596a2494465908708c18dfb0c773cfb6c54fc54
Nahsik16 <118239409+Nahsik16@users.noreply.github.com>
2024-02-18 15:28:02 +0530

    images in home

--- proj_1/src/components/Home.jsx
+++ proj_1/src/components/Home.jsx
@@ -1,9 +1,20 @@
 /* import React from 'react' */
+import "../styles/home.scss";
 
 const Home = () => {
-  return (
+  return (<><div className="main">
     <div>Home</div>
-  )
+    <div className="parent">
+
+      <div className="leftbox">
+
+      </div>
+      <div className="rightbox">
+    
+      </div>
+    </div>
+    </div>
+  </>)
 }
 
 export default Home
