3e8bfd15d0411c23622c6280275cfae8af471f4a
Mustafa <yusufmustufa@gmail.com>
2024-02-18 12:38:56 +0530

    Nav-1 (Header) basics complete

--- proj_1/src/components/Header.jsx
+++ proj_1/src/components/Header.jsx
@@ -1,15 +1,18 @@
 /* import React from 'react'*/
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import "../styles/header.scss";
+
 const Header = () => {
   return (
     <>
       <nav>
         <h1>MyFarma</h1>
         <main>
-          <Link href="/">Home</Link>
-          <Link href="/">Contact</Link>
-          <Link href="/">Sign-In</Link>
-
+          <Link to="/">Home</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to="/contact">Guidlines</Link>
+          <Link to="/contact">Nearest Seller</Link>
+          <Link to="/sign" className="Box">Sign-In</Link>
         </main>
       </nav>
     </>
