1559dd3c7238dca02b32f1ceb512e90aba36a326
Mustafa <yusufmustufa@gmail.com>
2024-02-18 18:34:13 +0530

    completed contactpage

--- proj_1/src/components/Contact.jsx
+++ proj_1/src/components/Contact.jsx
@@ -1,8 +1,33 @@
-/* import React from 'react'*/
+/* import React from "react"; */
+import "../styles/contact.scss";
+
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className="contact">
+      <main>
+        <h1> Contact Us</h1>
 
-export default Contact
+        <form>
+          <div>
+            <label>Name: </label>
+            <input type="text" required placeholder="Abc" />
+          </div>
+
+          <div>
+            <label>Email: </label>
+            <input type="email" required placeholder="Abc@xyz.com" />
+          </div>
+
+          <div>
+            <label>Message: </label>
+            <input type="text" placeholder="Tell us your query" />
+          </div>
+
+          <button type="submit">SUBMIT</button>
+        </form>
+      </main>
+    </div>
+  );
+};
+
+export default Contact;
